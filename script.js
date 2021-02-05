538a30562a3f9c0d079c7c0ae08f4ff9ddbb9f3a
Wina Tungmiharja <64743796+winatungmiharja@users.noreply.github.com>
2021-02-05 14:34:47 +0900

    added random picker

---
+++ script.js
@@ -0,0 +1,30 @@
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var css = document.querySelector("h3");
+var body = document.getElementById("gradient");
+var btn = document.getElementById("random");
+
+
+
+function print(){
+	css.textContent = color1.value + " " + color2.value;
+}
+print();
+function setGradient(){
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ " )";
+	print();
+}
+btn.onclick=function randomColour() {
+	color1.value = "#" + Math.floor(Math.random()*16777215).toString(16);
+	color2.value = "#" + Math.floor(Math.random()*16777215).toString(16);
+	setGradient();
+}
+
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
